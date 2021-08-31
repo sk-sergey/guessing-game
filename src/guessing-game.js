07780714fb244cc2ba11e07789e1127baaf5ef54
sk-sergey <pikachu.from.ch@gmail.com>
2021-08-31 14:28:58 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
-    
+    constructor() {
+        this.range = [null,null];
+        this.candidate = 0;
+    }
+
 
     setRange(min, max) {
-
+        this.range[0] = min
+        this.range[1] = max
     }
 
     guess() {
-
+        return this.candidate = Math.round((this.range[1] + this.range[0]) / 2)
     }
 
     lower() {
-
+        this.setRange(this.range[0], this.candidate)
     }
 
     greater() {
-
+        this.setRange(this.candidate,this.range[1] )        
     }
 }
 
